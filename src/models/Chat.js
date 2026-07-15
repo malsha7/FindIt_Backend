e9701495d4b22ce2b5aa141e3e62896395e1321c
malsha7 <malsha74bopage@gmail.com>
2026-07-15 14:50:06 +0530

    Created chat model

---
+++ src/models/Chat.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        item: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Item",
+            required: true
+        },
+
+        message: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        isRead: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Chat", chatSchema);
